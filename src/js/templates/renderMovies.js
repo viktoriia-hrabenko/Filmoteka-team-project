5b45318da78e50dbac6a3815dc915b6c687fa6b3
Bolek <103572991+BoloMasta@users.noreply.github.com>
2022-11-16 00:09:16 +0100

    fix API
    getTrending() and getSearch() works

--- src/js/templates/renderMovies.js
+++ src/js/templates/renderMovies.js
@@ -2,27 +2,27 @@ import { refs } from '../refs/refs';
 
 export { renderMoviesList };
 
-const renderMoviesList = (movies) => {
-    refs.moviesList.innerHTML = '';
-    const markupMoviesList = movies
-        .map(movie => {
-            const markup = `
+const renderMoviesList = movies => {
+  refs.moviesList.innerHTML = '';
+  const markupMoviesList = movies
+    .map(movie => {
+      const markup = `
                 <div class="movie-card">
                     <li class="movie-card__item" id="${movie.id}">
-                        <a href="#" class="movie-card__link>
-                            <img class="movie-card__image"src="${movie.poster_path}" alt="${movie.poster_path}" />
+                        <a href="#" class="movie-card__link">
+                            <img class="movie-card__image" src="https://image.tmdb.org/t/p/w300${movie.poster_path}" alt="${movie.poster_path}" />
                                 <div class="info">
                                     <p class="info__item">${movie.original_title}</p>
-                                    <p class="info__item">${movie.genres.name}</p>
+                                    <p class="info__item">${movie.genres}</p>
                                     <p class="info__item">${movie.release_date}</p>
                                 </div>
                         </a>
                     </li>
                 <div> 
                 `;
-            return markup;
-        })
-        .join("");
-    
-    refs.moviesList.insertAdjacentHTML('beforeend', markupMoviesList);
-}
+      return markup;
+    })
+    .join('');
+
+  refs.moviesList.insertAdjacentHTML('beforeend', markupMoviesList);
+};
