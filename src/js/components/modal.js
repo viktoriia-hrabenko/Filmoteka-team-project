b1ba604b504baa20d787da921453a318e704786c
Bolek <103572991+BoloMasta@users.noreply.github.com>
2022-11-28 12:19:21 +0100

    modal fixed

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -11,14 +11,11 @@ import { Loader } from './loader';
 const loader = new Loader();
 const fetchApiMovies = new FetchApiMovies();
 
-const loader = new Loader();
-const fetchApiMovies = new FetchApiMovies();
-
 export function addToggleModal() {
   const refsModal = {
     modalOpen: document.querySelectorAll('[data-modal-open]'),
     modalClose: document.querySelector('[data-modal-close]'),
-    modalBackdrop: document.querySelector('.modal-movie__backdrop')
+    modalBackdrop: document.querySelector('.modal-movie__backdrop'),
   };
 
   function addModalListener() {
@@ -26,25 +23,25 @@ export function addToggleModal() {
       'modal-movie__backdrop--is-hidden'
     );
     window.addEventListener('keydown', onEscKeyPress);
-    //loader.on();
-  };
+    loader.on();
+  }
 
   function removeModalListener() {
     refsModal.modalBackdrop.classList.add('modal-movie__backdrop--is-hidden');
-  };
+  }
 
   function onBackdropClick(event) {
     if (event.target != event.currentTarget) {
       return;
-    };
+    }
     removeModalListener();
   }
 
   function onEscKeyPress(event) {
     if (event.code === 'Escape') {
       removeModalListener();
-    };
-  };
+    }
+  }
 
   refsModal.modalOpen.forEach(modal => {
     modal.addEventListener('click', addModalListener);
@@ -54,8 +51,7 @@ export function addToggleModal() {
 
   refsModal.modalBackdrop.addEventListener('click', onBackdropClick);
   MovieModalCreate();
-};
-
+}
 
 const modalMoviePoster = document.querySelector('.modal-movie__poster--img');
 const modalMovieTitle = document.querySelector('.modal-movie__title');
@@ -67,109 +63,109 @@ const modalMovieOriginalTitle = document.querySelector(
 );
 const modalMovieGenre = document.querySelector('.modal-movie__genre');
 const modalMovieDescription = document.querySelector('.modal-movie__text');
-const modalMovieButtonWatched = document.querySelector('.modal-movie__button--watched');
-const modalMovieButtonQueued = document.querySelector('.modal-movie__button--queue');
+const modalMovieButtonWatched = document.querySelector(
+  '.modal-movie__button--watched'
+);
+const modalMovieButtonQueued = document.querySelector(
+  '.modal-movie__button--queue'
+);
 
-function MovieModalCreate(){
-const createModalMovie = document.querySelectorAll('.movie-card__item');
+function MovieModalCreate() {
+  const createModalMovie = document.querySelectorAll('.movie-card__item');
 
-createModalMovie.forEach(movie => {
-  movie.addEventListener('click', event => {
-    modalMoviePoster.src = '';
-    modalMovieTitle.innerHTML = '';
-    modalMovieVote.innerHTML = '';
-    modalMovieVotes.innerHTML = '';
-    modalMoviePopularity.innerHTML = '';
-    modalMovieOriginalTitle.innerHTML = '';
-    modalMovieGenre.innerHTML = '';
-    modalMovieDescription.innerHTML = '';
+  createModalMovie.forEach(movie => {
+    movie.addEventListener('click', event => {
+      modalMoviePoster.src = '';
+      modalMovieTitle.innerHTML = '';
+      modalMovieVote.innerHTML = '';
+      modalMovieVotes.innerHTML = '';
+      modalMoviePopularity.innerHTML = '';
+      modalMovieOriginalTitle.innerHTML = '';
+      modalMovieGenre.innerHTML = '';
+      modalMovieDescription.innerHTML = '';
 
-    modalMovieButtonWatched.dataset.id = '';
-    modalMovieButtonQueued.dataset.id = '';
+      modalMovieButtonWatched.dataset.id = '';
+      modalMovieButtonQueued.dataset.id = '';
 
-    event.preventDefault();
+      event.preventDefault();
 
-    const movieId = movie.id;
-    const movieData = fetchApiMovies.getMovieDetails(movieId);
+      const movieId = movie.id;
+      const movieData = fetchApiMovies.getMovieDetails(movieId);
 
-    movieData.then(value => {
-      MovieModalMurkup(value);
+      movieData.then(value => {
+        MovieModalMurkup(value);
+      });
+      loader.off();
     });
-    loader.off();
+
+    let onWatched = false;
+    let onQueue = false;
+
+    load('watchedList')?.forEach(movies => {
+      if (movies.id == movie.id) {
+        onWatched = true;
+      }
+    });
+
+    load('queueList')?.forEach(movies => {
+      if (movies.id == movie.id) {
+        onQueue = true;
+      }
+    });
+
+    let watchedBtn = document.querySelector('.modal-movie__button--watched');
+    let queueBtn = document.querySelector('.modal-movie__button--queue');
+
+    if (onWatched) {
+      watchedBtn.innerHTML = 'On List';
+    }
+    if (onQueue) {
+      queueBtn.innerHTML = 'On List';
+    }
+
+    const checkIfOnList = (button, listType, listTypeText) => {
+      let watched;
+      if (listTypeText == undefined) return;
+      if (listTypeText === 'watched') {
+        watched = onWatched;
+      } else if (listTypeText === 'queue') {
+        watched = onQueue;
+      }
+      if (watched) {
+        removeFromLibrary(movie.id, listType);
+        button.innerHTML = `Add to ${listTypeText}`;
+      } else {
+        addToLibrary(movie.id, listType);
+        button.innerHTML = `Added`;
+      }
+      if (listTypeText === 'watched') {
+        onWatched = !onWatched;
+      } else if (listTypeText === 'queue') {
+        onQueue = !onQueue;
+      }
+    };
+
+    watchedBtn.addEventListener('click', () => {
+      checkIfOnList(watchedBtn, 'watchedList', 'watched');
+      if (
+        document.location.href.includes('library') &&
+        refs.moviesList.dataset.listtype === 'watched'
+      ) {
+        let tempLibraryList = load('watchedList');
+        getAllLibraryMovies(tempLibraryList, 'watchedList');
+      }
+    });
+    queueBtn.addEventListener('click', () => {
+      checkIfOnList(queueBtn, 'queueList', 'queue');
+      if (
+        document.location.href.includes('library') &&
+        refs.moviesList.dataset.listtype === 'queue'
+      ) {
+        let tempLibraryList = load('queueList');
+        getAllLibraryMovies(tempLibraryList, 'queueList');
+      }
+    });
   });
-
-  let onWatched = false;
-  let onQueue = false;
-  
-  load('watchedList')?.forEach(movies => {
-    if (movies.id == movie.id) {
-
-      onWatched = true;
-    }
-  });
-
-
-  load('queueList')?.forEach(movies => {
-    if (movies.id == movie.id) {
-
-      onQueue = true;
-    }
-  });
-
-  let watchedBtn = document.querySelector('.modal-movie__button--watched');
-  let queueBtn = document.querySelector('.modal-movie__button--queue');
-
-
-  if (onWatched) {
-    watchedBtn.innerHTML = 'On List';
-  }
-  if (onQueue) {
-    queueBtn.innerHTML = 'On List';
-  }
-
-  const checkIfOnList = (button, listType, listTypeText) => {
-    let watched;
-    if (listTypeText == undefined) return;
-    if (listTypeText === 'watched') {
-      watched = onWatched;
-    } else if (listTypeText === 'queue') {
-      watched = onQueue;
-    }
-    if (watched) {
-      removeFromLibrary(movie.id, listType);
-      button.innerHTML = `Add to ${listTypeText}`;
-    } else {
-      addToLibrary(movie.id, listType);
-      button.innerHTML = `Added`;
-    }
-    if (listTypeText === 'watched') {
-      onWatched = !onWatched;
-    } else if (listTypeText === 'queue') {
-      onQueue = !onQueue;
-    }
-  };
-
-  watchedBtn.addEventListener('click', () => {
-    checkIfOnList(watchedBtn, 'watchedList', 'watched');
-    if (
-      document.location.href.includes('library') &&
-      refs.moviesList.dataset.listtype === 'watched'
-    ) {
-      let tempLibraryList = load('watchedList');
-      getAllLibraryMovies(tempLibraryList, 'watchedList');
-    }
-  });
-  queueBtn.addEventListener('click', () => {
-    checkIfOnList(queueBtn, 'queueList', 'queue');
-    if (
-      document.location.href.includes('library') &&
-      refs.moviesList.dataset.listtype === 'queue'
-    ) {
-      let tempLibraryList = load('queueList');
-      getAllLibraryMovies(tempLibraryList, 'queueList');
-    }
-  });
-});
 }
 
 function MovieModalMurkup({
@@ -181,19 +177,16 @@ function MovieModalMurkup({
   overview,
   genres,
   vote_average,
-  vote_count
+  vote_count,
 }) {
   modalMoviePoster.src = `https://image.tmdb.org/t/p/w500${poster_path}`;
   modalMovieTitle.innerHTML = title;
-  modalMovieVote.innerHTML = vote_average;
+  modalMovieVote.innerHTML = vote_average.toFixed(1);
   modalMovieVotes.innerHTML = vote_count;
-  modalMoviePopularity.innerHTML = popularity;
+  modalMoviePopularity.innerHTML = popularity.toFixed(1);
   modalMovieOriginalTitle.innerHTML = original_title;
-  modalMovieGenre.innerHTML = genres.map(genre => genre.name)
-  .join(', ');
+  modalMovieGenre.innerHTML = genres.map(genre => genre.name).join(', ');
   modalMovieDescription.innerHTML = overview;
   modalMovieButtonWatched.dataset.id = id;
   modalMovieButtonQueued.dataset.id = id;
-};
-
-
+}
